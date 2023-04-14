3a0f00f3d83d8ff1c8419164a9163b4c8e8f0346
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-04-14 19:45:54 +0000

    create-tables

--- school/school.sql
+++ school/school.sql
@@ -16,4 +16,22 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE sections(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
